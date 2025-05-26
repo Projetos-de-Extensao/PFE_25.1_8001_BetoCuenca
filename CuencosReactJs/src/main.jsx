02f5bb1063d3ac123d47b77e20465bee33659643
Anderson Lima ☕ <72580323+andersonodev@users.noreply.github.com>
2025-05-26 03:48:21 -0300

    estrutura correta do projeto

--- CuencosReactJs/src/main.jsx
+++ CuencosReactJs/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import './styles/mobile.css';
+import { setupResourceErrorMonitoring } from './lib/debug-utils';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+// Iniciar monitoramento de erros 404 em ambiente de desenvolvimento
+if (process.env.NODE_ENV === 'development') {
+  setupResourceErrorMonitoring();
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
--- cuencos/utils.js
+++
@@ -1,4 +0,0 @@
-// Função para unir classes de forma condicional (usado no componente pagination)
-export function cn(...classes) {
-  return classes.filter(Boolean).join(" ");
-}
